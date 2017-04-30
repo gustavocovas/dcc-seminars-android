4b7bd5feaa90e77e9ef6f8a451858a94631ed0a2
Gustavo Covas <gtcovas@gmail.com>
2017-04-30 12:39:24 -0300

    Generate project and prototype navigation between activities

---
+++ app/src/main/java/br/usp/ime/dcc/seminariosdcc/LoginActivity.java
@@ -0,0 +1,19 @@
+package br.usp.ime.dcc.seminariosdcc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+//    TODO: Everything! :)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
