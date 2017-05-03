dc4eea4bdeee6ccb735f1749807acf343ee2f7a5
Gustavo Covas <gtcovas@gmail.com>
2017-05-03 13:29:19 -0300

    Remove fragments and create StudentSignInSignUp activity

--- app/src/main/java/br/usp/ime/dcc/seminariosdcc/LoginActivity.java
+++ app/src/main/java/br/usp/ime/dcc/seminariosdcc/LoginActivity.java
@@ -1,12 +1,13 @@
 package br.usp.ime.dcc.seminariosdcc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
-//    TODO: Everything! :)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +15,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        Button studentButton = (Button) findViewById(R.id.login_student_button);
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent studentSignInSignUp = new Intent(LoginActivity.this, StudentSignInSignUpActivity.class);
+                startActivity(studentSignInSignUp);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/br/usp/ime/dcc/seminariosdcc/StudentSignInSignUpActivity.java
@@ -0,0 +1,13 @@
+package br.usp.ime.dcc.seminariosdcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentSignInSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_sign_in_sign_up);
+    }
+}
